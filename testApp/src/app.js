b69fc5b1a646c0aec306db7c117923d7117083ad
Halochkin <maksgalochkin2@gmail.com>
2021-01-12 19:07:51 +0200

    post request

--- testApp/src/app.js
+++ testApp/src/app.js
@@ -248,10 +248,16 @@ class App extends HTMLElement {
 
   async postData(url = '', data = {}) {
     const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
       headers: {
         'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
     return await response.json();
