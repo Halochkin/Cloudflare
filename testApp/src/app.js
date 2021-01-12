62d10c2f0576c4a47ace34832dd13ab4bc567cad
Halochkin <maksgalochkin2@gmail.com>
2021-01-12 14:07:53 +0200

    request

--- testApp/src/app.js
+++ testApp/src/app.js
@@ -249,16 +249,9 @@ class App extends HTMLElement {
   async postData(url = '', data = {}) {
     const response = await fetch(url, {
       headers: {
-        // 'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      // mode: 'no-cors', // no-cors, *cors, same-origin
-      // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      // credentials: 'same-origin', // include, *same-origin, omit
-      // redirect: 'follow', // manual, *follow, error
-      // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      // body: data // body data type must match "Content-Type" header
       body: data // body data type must match "Content-Type" header
     });
     return response.json();// parses JSON response into native JavaScript objects
@@ -317,12 +310,9 @@ class App extends HTMLElement {
       this.previousSessions.set(result, this.sessionTrack);
 
 
-      let data = JSON.stringify({halochkin: "abcde"});
-      let res = await this.postData("https://typing-race.maksgalochkin2.workers.dev/json", data);
+      let data = JSON.stringify({sessionId: Date.now(), result: result, history: this.sessionTrack});
 
-      let parsedRes = await res.json();
-
-      console.log("RES", res, parsedRes);
+      await this.postData("https://typing-race.maksgalochkin2.workers.dev/json", data);
 
       return this.refresh();
     }
