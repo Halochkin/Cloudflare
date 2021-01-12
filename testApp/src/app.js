e96b2fe138dd84ff3a1d3990f9b31e6f2e7343db
Halochkin <maksgalochkin2@gmail.com>
2021-01-12 15:56:51 +0200

    request

--- testApp/src/app.js
+++ testApp/src/app.js
@@ -254,7 +254,7 @@ class App extends HTMLElement {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
       body: data // body data type must match "Content-Type" header
     });
-    return await response.json();// parses JSON response into native JavaScript objects
+    return response// parses JSON response into native JavaScript objects
 
   }
 
