968c09215b28efe687d743942e7ce99f410f839f
Halochkin <maksgalochkin2@gmail.com>
2021-01-12 19:37:49 +0200

    post request

--- testApp/src/app.js
+++ testApp/src/app.js
@@ -259,7 +259,7 @@ class App extends HTMLElement {
       redirect: 'follow', // manual, *follow, error
       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
 
-      body: data.substring(1,data.length-2) // body data type must match "Content-Type" header
+      body: data // body data type must match "Content-Type" header
 
 
 
