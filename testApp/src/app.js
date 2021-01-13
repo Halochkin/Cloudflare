25c904a0b8821b88c09dd5c472621b84e16a5298
Halochkin <maksgalochkin2@gmail.com>
2021-01-13 12:10:01 +0200

    post request

--- testApp/src/app.js
+++ testApp/src/app.js
@@ -2,7 +2,7 @@ class App extends HTMLElement {
   constructor() {
     super();
     // this.expression = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus maximus erat. Praesent luctus, quam nec consequat sagittis, justo erat iaculis mauris, sodales tristique odio enim non erat. Curabitur dapibus fermentum tellus ac viverra. Sed eros lorem, bibendum sit amet nisl sit amet, ultricies posuere ipsum. "
-    this.expression = "l";
+    this.expression = "Lorem ipsum";
     this.attachShadow({mode: "open"});
     this.shadowRoot.innerHTML = `
 <style>
