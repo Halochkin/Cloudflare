f4f8619b29d8a584b167a0c34a08872fa8d68865
Halochkin <maksgalochkin2@gmail.com>
2021-01-29 01:20:11 +0200

    big push

--- StateManagement(TypingRacerApp)/src/Reducers.js
+++ StateManagement(TypingRacerApp)/src/Reducers.js
@@ -79,8 +79,10 @@ export class Reducers {
   }
 
   static getAllSessions(state) {
-    this.doRequest('GET', "https://typing-app.maksgalochkin2.workers.dev/getsessions").then(data => this.renderSessions(data));
-    return state
+    setTimeout(() => {
+      this.doRequest('GET', "https://typing-app.maksgalochkin2.workers.dev/getsessions").then(data => this.renderSessions(data));
+      return state
+    }, 100)
   }
 }
 
