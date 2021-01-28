393d3a114426454f4d3891b1f8291b2b4b8479a3
Halochkin <maksgalochkin2@gmail.com>
2021-01-29 00:56:00 +0200

    big push

--- StateManagement(TypingRacerApp)/src/app.js
+++ StateManagement(TypingRacerApp)/src/app.js
@@ -241,9 +241,9 @@ margin-top: -2vw;
     }
     if (body)
       options.body = body;
-    // let res = await fetch(path, options);
-    // let result =  await res.json();
-    return await fetch(path, options).then(response => response.json()).then(data=> data);
+    let res = await fetch(path, options);
+    let result =  await res.json();
+    // return await fetch(path, options).then(response => response.json()).then(data=> data);
    }
 
   countWPM(state, durationMs) {
